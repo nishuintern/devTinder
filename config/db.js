d2a459a5b338f79dfc5df8485aae0fe3913f3190
nishuintern <nishuintern21@gmail.com>
2025-04-11 21:28:52 +0530

    up

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   await mongoose.connect(
-    "mongodb://localhost:27017/devTinders"
+    // "mongodb://localhost:27017/devTinders"
+    "mongodb+srv://nishus877:IfN6f68E0jZqy7Sj@devtinder.ocvkb99.mongodb.net/devTinder"
   );
 };
 module.exports = dbConnect;

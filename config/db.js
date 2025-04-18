d9f5bab0ff3c6905d25337701c21baa5a6da0b17
nishuintern <nishuintern21@gmail.com>
2025-04-18 14:43:02 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   await mongoose.connect(
     // "mongodb://localhost:27017/devTinders"
-    "mongodb+srv://nishus877:IfN6f68E0jZqy7Sj@devtinder.ocvkb99.mongodb.net/devTinder"
+    // "mongodb+srv://nishus877:IfN6f68E0jZqy7Sj@devtinder.ocvkb99.mongodb.net/devTinder"
+    process.env.DB_CONNECTION_SECRET
   );
 };
 module.exports = dbConnect;
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const dbConnect = require("../config/db");
 const cookieParser = require("cookie-parser");
 require('dotenv').config()
-const PORT = 8000;
+const PORT = process.env.PORT;
 const cors = require("cors");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());

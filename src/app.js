e7b7e339540f4cd6b67656c222d500a6ea5cd923
nishuintern <nishuintern21@gmail.com>
2025-05-19 21:14:50 +0530

    update

--- src/app.js
+++ src/app.js
@@ -6,14 +6,7 @@ const http = require("http");
 require("dotenv").config();
 const PORT = 7777;
 const cors = require("cors");
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://devtinder-web-zkxd.onrender.com"],
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 // const corsOptions = {

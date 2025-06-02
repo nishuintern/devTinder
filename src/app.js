fb0a900f09c6e54ed5b2f366dce936d1e587ad37
nishuintern <nishuintern21@gmail.com>
2025-06-02 22:54:48 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "http://localhost:5173", // local frontend
-    "https://devtinder-web-zkxd.onrender.com", // <-- replace with your deployed frontend URL
+    "https://dev-tinder-web.netlify.app", // <-- replace with your deployed frontend URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],

05148c58802ecff23c3bb5abfd43c6036ff01b63
nishuintern <nishuintern21@gmail.com>
2025-06-03 11:21:53 +0530

    u

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,12 +18,14 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password: passwordHash,
     });
-   const savedUser= await user.save();
-   const token = await savedUser.getJWT();
-      res.cookie("token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 8 * 3600000),
-      });
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
     res.json({
       message: "User Added Successfully!!!",
       data: savedUser,
